59469977619e80e38f9a355f578e1c74ec095aa5
Meghan <stlmeghan@yahoo.com>
2022-08-06 00:48:49 -0500

    almost done

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListExercises
 {
@@ -6,7 +7,56 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Lists!");
+            List<int> nums = new List<int> { 3, 8, 2, 1, 3, 9, 8, 7, 5, 4 };
+            MessWithLists.SumEvens(nums);
+
+            List<string> words = new List<string> { "bangs", "five", "trees", "pig", "to", "cigar", "balloon", "grave", "as", "shin", "camp", "log"};
+            Console.WriteLine("Enter a word length: ");
+            string searchTerm = Console.ReadLine();
+            int searchNumber = int.Parse(searchTerm);
+            MessWithLists.PrintRandomNumberLetters(words, searchNumber);
+        }
+        public class MessWithLists
+        {
+            public static int SumEvens(List<int> args)
+            {
+                int sum = 0;
+
+                foreach (int number in args)
+                {
+                    if (number % 2 == 0)
+                    {
+                        sum += number;
+                    }
+                }
+                Console.WriteLine(sum);
+                return sum;
+
+            }
+
+            public static void PrintFiveLetters(List<string> args)
+            {
+                foreach (string word in args)
+                {
+                    if (word.Length == 5)
+                    {
+                        Console.WriteLine(word);
+                    }
+                }
+            }
+
+            public static void PrintRandomNumberLetters(List<string> args, int length)
+            {
+                foreach (string word in args)
+                {
+                    if (word.Length == length)
+                    {
+                        Console.WriteLine(word);
+                    }
+                }
+            }
         }
     }
 }
+
+
--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,13 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            string drSeuss = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+
+            string[] words = drSeuss.Split(' ');
+            string[] sentences = drSeuss.Split('.');
+
+            Console.WriteLine(string.Join("|", words));
+            Console.WriteLine(string.Join("|", sentences));
         }
     }
 }
